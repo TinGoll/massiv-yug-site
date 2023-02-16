ea82597eea791a90539a3f5634934ee082cf80f1
Roman <roma.oliynyk83@gmail.com>
2023-02-17 01:02:38 +0300

    c 17

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import Layout from "../layout/Layout";
-import HeaderWrapper from "../components/header/header-wrapper/header-wrapper";
+
 import Banner from "../components/banner/banner";
 import Product from "../components/product/product";
 import Footer from "../components/footer/footer";
 import AboutCompany from "../components/about-company/about-company";
 import BlockDivider from "../components/block-divider/block-divider";
 import Promotion from "../components/promotion/promotion";
+import HeaderWrapper from "../components/header/header-wrapper/header-wrapper";
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
